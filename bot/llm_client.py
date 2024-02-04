3e20093ad4934ef1ebdf0302598e253f6969b8d6
Dima Timofeev <dimkat@gmail.com>
2024-02-04 11:17:40 -0800

    enforce types

--- bot/llm_client.py
+++ bot/llm_client.py
@@ -2,10 +2,16 @@ import os
 
 from dotenv import load_dotenv
 from langchain_core.language_models.chat_models import BaseChatModel
+from langchain_core.messages import SystemMessage
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.output_parsers.base import BaseOutputParser
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts.base import BasePromptTemplate
+from langchain_core.runnables.base import RunnableSequence
 from langchain_openai import ChatOpenAI
+from langchain.prompts import HumanMessagePromptTemplate
 
-API_KEY_ENV_VAR_NAME = "OPENAI_API_KEY"
+API_KEY_ENV_VAR_NAME: str = "OPENAI_API_KEY"
 
 # Current version expects .env file to be in the same directory as the bot.
 # The .env file contains the following:
@@ -16,31 +22,36 @@ API_KEY_ENV_VAR_NAME = "OPENAI_API_KEY"
 # TODO: Add support for multiple users with their own API keys.
 load_dotenv()
 
-template = ChatPromptTemplate.from_messages([
-    ("system", "You are a professional proofreader, you are here to help me with rewriting texts. "
-     "I will provide you texts and I would like you to review and rewerite them, and fix "
-     "any style, spelling, grammar, or punctuation errors. Once you have finished reviewing, "
-     "provide me with corrected text."),
-    ("human", "The text to proofread, rewrite, and fix is: {text}"),
-])
+SYSTEM_MESSAGE: str = (
+    "You are a professional proofreader, you are here to help me with rewriting texts. "
+    "I will provide you texts and I would like you to review and rewerite them, and fix "
+    "any style, spelling, grammar, or punctuation errors. Once you have finished reviewing, "
+    "provide me with corrected text."
+)
+prompt: BasePromptTemplate = ChatPromptTemplate.from_messages(
+    [
+        SystemMessage(content=SYSTEM_MESSAGE),
+        HumanMessagePromptTemplate.from_template(
+            "The text to proofread, rewrite, and fix is: {text}"
+        ),
+    ]
+)
 
 
 class LLMClient:
     def __init__(self) -> None:
         api_key: str = os.getenv(API_KEY_ENV_VAR_NAME)
         if api_key is None:
-            raise ValueError(
-                f"Environment variable {API_KEY_ENV_VAR_NAME} is not set.")
-        self._model: BaseChatModel = ChatOpenAI(openai_api_key=api_key)
+            raise ValueError(f"Environment variable {API_KEY_ENV_VAR_NAME} is not set.")
+        self._llm: BaseChatModel = ChatOpenAI(openai_api_key=api_key)
 
     def proofread_and_rewrite(self, text: str) -> str:
-        prompt = template.format_prompt(text=text).to_messages()
-        chain = self._model.invoke(prompt)
-        return chain.content
+        output_parser: BaseOutputParser = StrOutputParser()
+        chain: RunnableSequence = prompt | self._llm | output_parser
+        return chain.invoke({"text": text})
 
 
 if __name__ == "__main__":
     client = LLMClient()
-    text = ("I really appreciate the last lesson from yeaterday. "
-            "I would like to learn more from you during this semester.")
+    text = "Big storm, San Francisco bay, very fierce, ships go up and down!"
     print(client.proofread_and_rewrite(text))
